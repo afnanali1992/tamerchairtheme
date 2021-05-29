782bca3cc4daa9fccc6dcd9c7c3d1ea35f8a9017
Afnan Bayazed <abayazid2013@gmail.com>
2021-05-29 10:59:42 +0300

    first commit

---
+++ footer.php
@@ -0,0 +1,69 @@
+<footer class="theme-footer-one main-p-color pt-130">
+				<div class="top-footer">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-3 col-sm-6 col-12 footer-about-widget">
+								<a href="index.html" class="logo"><img src="images/logo/logo.svg" alt=""></a>
+								<a href="#" class="email">rogan.inc@company.com</a>
+								<a href="#" class="phone">720.661.2231</a>
+								
+							</div> <!-- /.about-widget -->
+							<div class="col-lg-3 col-lg-3 col-sm-6 col-12 footer-list">
+								<h5 class="footer-title">Services</h5>
+								<ul>
+									<li><a href="#">Web Design</a></li>
+									<li><a href="#">Development</a></li>
+									<li><a href="#">Wordpress</a></li>
+									<li><a href="#">Online Marketing</a></li>
+									<li><a href="#">Content</a></li>
+								</ul>
+							</div> <!-- /.footer-recent-post -->
+							<div class="col-lg-3 col-sm-6 col-12 footer-list">
+								<h5 class="footer-title">About us</h5>
+								<ul>
+									<li><a href="about-standard.html">About us</a></li>
+									<li><a href="portfolio-v1.html">Work Portfolio</a></li>
+									<li><a href="team-v1.html">Team</a></li>
+									<li><a href="#">Plan & Pricing</a></li>
+									<li><a href="blog-v1.html">News</a></li>
+								</ul>
+							</div> <!-- /.footer-list -->
+							<div class="col-lg-3 col-lg-2 col-sm-6 col-12 footer-information">
+								<h5 class="footer-title">Our Address</h5>
+								<p>00 Orville Road Apt. 728 <br>California, USA</p>
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div> <!-- /.row -->
+					</div> <!-- /.container -->
+				</div> <!-- /.top-footer -->
+				
+				<div class="container">
+					<div class="bottom-footer-content">
+						<p>&copy; 2019 copyright all right reserved</p>
+					</div> <!-- /.bottom-footer -->
+				</div>
+			</footer> <!-- /.theme-footer-one -->
+			
+
+	        
+
+	        <!-- Scroll Top Button -->
+			<button class="scroll-top tran3s">
+				<i class="fa fa-angle-up" aria-hidden="true"></i>
+			</button>
+
+			
+
+
+		<!-- Optional JavaScript _____________________________  -->
+
+    	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    	<!-- jQuery -->
+	<?php wp_footer();?>
+		</div> <!-- /.main-page-wrapper -->
+	</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,53 @@
+<?php
+
+
+function load_stylesheets()
+{
+    wp_register_style('style',get_template_directory_uri().'/css/style.css',array(),1,'all');
+    wp_enqueue_style('style');
+
+    wp_register_style('responsive',get_template_directory_uri().'/css/responsive.css',array(),1,'all');
+    wp_enqueue_style('responsive');
+}
+ add_action('wp_enqueue_scripts','load_stylesheets');
+
+ function addjs(){
+     wp_register_script('jquery',get_template_directory_uri().'/vendor/jquery.2.2.3.min.js',array(),1,1,1);
+     wp_enqueue_script('jquery');
+
+     wp_register_script('popper',get_template_directory_uri().'/vendor/popper.js/popper.min.js',array(),1,1,1);
+     wp_enqueue_script('popper');
+
+     wp_register_script('bootstrap',get_template_directory_uri().'/vendor/bootstrap/js/bootstrap.min.js',array(),1,1,1);
+     wp_enqueue_script('bootstrap');
+
+     wp_register_script('mega-menu',get_template_directory_uri().'/vendor/mega-menu/assets/js/custom.js',array(),1,1,1);
+     wp_enqueue_script('mega-menu');
+
+     wp_register_script('aos-next',get_template_directory_uri().'/vendor/aos-next/dist/aos.js',array(),1,1,1);
+     wp_enqueue_script('aos-next');
+
+     wp_register_script('WOW-master',get_template_directory_uri().'/vendor/WOW-master/dist/wow.min.js',array(),1,1,1);
+     wp_enqueue_script('WOW-master');
+
+     wp_register_script('owl-carousel',get_template_directory_uri().'/vendor/owl-carousel/owl.carousel.min.js',array(),1,1,1);
+     wp_enqueue_script('owl-carousel');
+
+     wp_register_script('ajaxchimp',get_template_directory_uri().'/vendor/ajaxchimp/jquery.ajaxchimp.min.js',array(),1,1,1);
+     wp_enqueue_script('ajaxchimp');
+
+     wp_register_script('fancybox',get_template_directory_uri().'/vendor/fancybox/dist/jquery.fancybox.min.js',array(),1,1,1);
+     wp_enqueue_script('fancybox');
+
+     wp_register_script('tilt',get_template_directory_uri().'/vendor/tilt.jquery.js',array(),1,1,1);
+     wp_enqueue_script('tilt');
+
+     wp_register_script('theme',get_template_directory_uri().'/js/theme.js',array(),1,1,1);
+     wp_enqueue_script('theme');
+
+     wp_register_script('custom',get_template_directory_uri().'/custom.js',array(),1,1,1);
+     wp_enqueue_script('custom');
+ }
+
+ add_action('init', 'addjs');
+ 
