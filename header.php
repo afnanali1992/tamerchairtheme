abed892d3bf1812200335c8127304a229815134e
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-06-02 16:40:08 +0300

    Fix Header with wordpress menu

--- header.php
+++ header.php
@@ -74,226 +74,33 @@
             <header>
 			<div class="theme-main-menu theme-menu-two main-p-color sticky-menu">
 				<div class="d-flex align-items-center">
+				<!-- LOGO -->
 					<div class="logo"><a href="index.html"><img src="images/logo/logo.svg" alt=""></a></div>
 					<div class="right-content ml-auto order-lg-3">
 						<a href="contact-v1.html" class="theme-btn line-button-one button-rose contact-button button-white-bg">Contact us</a>
 					</div>
+
 					<nav id="mega-menu-holder" class="navbar navbar-expand-lg order-lg-2">
 						<div  class="container nav-container">
 							<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						        <i class="flaticon-setup"></i>
 						    </button>
 						   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-						   		<ul class="navbar-nav">
+						   		<?php wp_nav_menu(
+									   array(
+										   'theme_location' => 'header',
+										   'container'=> 'ul',
+										   'menu_class' => 'navbar-nav',
+										   'start_depth'=>  1,
+										   
+									   )
+									   ); ?>
+								   
+								   <!-- <ul class="navbar-nav">
 								    <li class="nav-item dropdown mega-dropdown-holder active">
 							            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Home</a>
-							            <ul class="dropdown-menu mega-container">
-								            <li>
-									            <div class="clearfix">
-									              	<div class="row">
-										                <div class="col-lg-2 col-md-4 col-sm-6 mt-3 mb-3 mt-lg-3 mb-lg-3">
-										                   	<a href="index.html" class="img-box">
-										                   		<span class="img"><img src="images/menu-img/home-1.jpg" alt=""></span>
-										                   		<span class="text">Creative Agency 2</span>
-										                   	</a>
-										                </div>
-										                <div class="col-lg-2 col-md-4 col-sm-6 mt-3 mb-3 mt-lg-3 mb-lg-3">
-										                   	<a href="index-2.html" class="img-box">
-										                   		<span class="img"><img src="images/menu-img/home-2.jpg" alt=""></span>
-										                   		<span class="text">SEO Agency</span>
-										                   	</a>
-										                </div>
-										                <div class="col-lg-2 col-md-4 col-sm-6 mt-3 mb-3 mt-lg-3 mb-lg-3 ml-auto">
-										                   	<a href="index-3.html" class="img-box">
-										                   		<span class="img"><img src="images/menu-img/home-3.jpg" alt=""></span>
-										                   		<span class="text">Business Agency</span>
-										                   	</a>
-										                </div>
-										                <div class="col-lg-2 col-md-4 col-sm-6 mt-3 mb-3 mt-lg-3 mb-lg-3">
-										                   	<a href="index-4.html" class="img-box">
-										                   		<span class="img"><img src="images/menu-img/home-4.jpg" alt=""></span>
-										                   		<span class="text">Creative Agency 3</span>
-										                   	</a>
-										                </div>
-										                <div class="col-lg-2 col-md-4 col-sm-6 mt-3 mb-3 mt-lg-3 mb-lg-3">
-										                   	<a href="index-5.html" class="img-box">
-										                   		<span class="img"><img src="images/menu-img/home-5.jpg" alt=""></span>
-										                   		<span class="text">Business Agency 2</span>
-										                   	</a>
-										                </div>
-										                <div class="col-lg-2 col-md-4 col-sm-6 mt-3 mb-3 mt-lg-3 mb-lg-3 mr-auto">
-										                	<a href="#" class="img-box">
-										                		<span class="img"><img src="images/menu-img/cs.jpg" alt=""></span>
-										                		<span class="text">Coming Soon</span>
-										                	</a>
-										                </div>
-									            	</div>
-									          	</div>
-									        </li>
-							        	</ul>
 							        </li>
-								    <li class="nav-item dropdown">
-							            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Pages.</a>
-							            <ul class="dropdown-menu">
-								            <li class="dropdown-submenu dropdown">
-								              	<a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">About Us</a>
-								              	<ul class="dropdown-menu">
-									                <li><a href="about-agency.html" class="dropdown-item">About Us agency</a></li>
-									                <li><a href="about-standard.html" class="dropdown-item">About Us standard</a></li>
-								                </ul>
-								            </li>
-								            <li class="dropdown-submenu dropdown">
-								              	<a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">Project</a>
-								              	<ul class="dropdown-menu">
-									                <li><a href="portfolio-v1.html" class="dropdown-item">Project Full width</a></li>
-									                <li><a href="portfolio-v4.html" class="dropdown-item">Project minimal</a></li>
-									                <li><a href="portfolio-v5.html" class="dropdown-item">Project with sidebar</a></li>
-									                <li><a href="portfolio-v2.html" class="dropdown-item">Project masonry full</a></li>
-									                <li><a href="portfolio-v3.html" class="dropdown-item">Project Standard</a></li>
-									                <li class="dropdown-submenu dropdown">
-								                    	<a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">Project Details</a>
-									                    <ul class="dropdown-menu">
-									                    	<li><a href="portfolio-details-v1.html" class="dropdown-item">Project Details One</a></li>
-									                    	<li><a href="portfolio-details-v2.html" class="dropdown-item">Project Details Two</a></li>
-									                    	<li><a href="portfolio-details-v3.html" class="dropdown-item">Project Details Three</a></li>
-									                    	<li><a href="portfolio-details-v4.html" class="dropdown-item">Project Details Four</a></li>
-									                    	<li><a href="portfolio-details-v5.html" class="dropdown-item">Project Details five</a></li>
-									                    </ul>
-								                  	</li>
-								                </ul>
-								            </li>
-								            <li class="dropdown-submenu dropdown">
-								              	<a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">Services</a>
-								              	<ul class="dropdown-menu">
-									                <li><a href="service-v1.html" class="dropdown-item">Service creative</a></li>
-									                <li><a href="service-v2.html" class="dropdown-item">service minimal</a></li>
-									                <li><a href="service-v3.html" class="dropdown-item">service modern</a></li>
-								                </ul>
-								            </li>
-								            <li class="dropdown-submenu dropdown">
-								              	<a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">Team</a>
-								              	<ul class="dropdown-menu">
-									                <li><a href="team-v1.html" class="dropdown-item">Team Business</a></li>
-									                <li><a href="team-v2.html" class="dropdown-item">Team minimal</a></li>
-									                <li><a href="team-v3.html" class="dropdown-item">Team standard</a></li>
-								                </ul>
-								            </li>
-							              	<li class="dropdown-submenu dropdown">
-								              	<a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">Contact Us</a>
-								              	<ul class="dropdown-menu">
-									                <li><a href="contact-v1.html" class="dropdown-item">Contact Agency</a></li>
-									                <li><a href="contact-v2.html" class="dropdown-item">Contact Minimal</a></li>
-									                <li><a href="contact-v3.html" class="dropdown-item">Contact standard</a></li>
-								                </ul>
-								            </li>
-							             	<li><a class="dropdown-item" href="error-v1.html">Error</a></li>
-							            </ul> <!-- /.dropdown-menu -->
-							        </li>
-							        <li class="nav-item dropdown icon-menu">
-							            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Features.</a>
-							            <ul class="dropdown-menu">
-								            <li class="dropdown-submenu dropdown">
-								              	<a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">
-								              		<i class="fa fa-shopping-bag icon" aria-hidden="true"></i>
-								              		Shop
-								              	</a>
-								              	<ul class="dropdown-menu">
-									                <li><a href="shop-full-width.html" class="dropdown-item">Shop Full width</a></li>
-									                <li><a href="shop-sidebar.html" class="dropdown-item">Shop With sidebar</a></li>
-									                <li><a href="shop-details.html" class="dropdown-item">Single product</a></li>
-								                </ul>
-								            </li>
-								            <li class="dropdown-submenu dropdown">
-								            	<a href="#" class="dropdown-item dropdown-toggle" data-toggle="dropdown">
-								            		<i class="fa fa-question-circle icon" aria-hidden="true"></i>
-								            		Faq
-								            	</a>
-								            	<ul class="dropdown-menu">
-									                <li><a href="faq-v2.html" class="dropdown-item">Faq Standard</a></li>
-									                <li><a href="faq-v1.html" class="dropdown-item">Faq Minimal</a></li>
-								                </ul>
-								            </li>
-								            <li>
-								            	<a href="checkout.html" class="dropdown-item">
-								            		<i class="fa fa-check-circle icon" aria-hidden="true"></i>
-								            		Checkout
-								            	</a>
-								        	</li>
-									        <li>
-									        	<a href="cart.html" class="dropdown-item">
-									        		<i class="fa fa-shopping-cart icon" aria-hidden="true"></i>
-									        		Cart
-									        	</a>
-									        </li>
-									        <li class="dropdown-submenu dropdown">
-								              	<a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">
-								              		<i class="fa fa-user icon" aria-hidden="true"></i>
-								              		User pages
-								              	</a>
-								              	<ul class="dropdown-menu">
-									                <li><a href="signin-standard.html" class="dropdown-item">Login Standard</a></li>
-									                <li><a href="signin-minimal.html" class="dropdown-item">Login Minimal</a></li>
-									                <li><a href="sign-up-standard.html" class="dropdown-item">Sign-Up Standard</a></li>
-									                <li><a href="sign-up-minimal.html" class="dropdown-item">Sign-Up Minimal</a></li>
-								                </ul>
-								            </li>
-							            </ul> <!-- /.dropdown-menu -->
-							        </li>
-							        <li class="nav-item dropdown mega-dropdown-holder">
-							            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">elements.</a>
-							            <ul class="dropdown-menu mega-container">
-								            <li>
-								                <div class="container">
-								                  	<div class="row">
-									                    <div class="col-lg-3 col-md-3 mt-md-3 mb-md-3 mt-lg-3 mb-lg-3">
-									                    	<h6 class="mega-menu-title">Element 1</h6>
-									                    	<ul class="list-unstyled mega-dropdown-list">
-										                        <li><a href="ui-testimonial.html"><img src="images/menu-icon/icon1.svg" alt="" class="svg">Testimonials</a></li>
-										                        <li><a href="ui-pricing-table.html"><img src="images/menu-icon/icon2.svg" alt="" class="svg">Pricing Plan</a></li>
-										                        <li><a href="ui-team.html"><img src="images/menu-icon/icon3.svg" alt="" class="svg">Team</a></li>
-										                        <li><a href="ui-dropdown.html"><img src="images/menu-icon/icon4.svg" alt="" class="svg">Dropdown</a></li>
-										                        <li><a href="ui-tooltips.html"><img src="images/menu-icon/icon5.svg" alt="" class="svg">Tooltips</a></li>
-										                        <li><a href="ui-fancy-text.html"><img src="images/menu-icon/icon6.svg" alt="" class="svg">Fancy Text</a></li>
-										                        <li><a href="ui-header.html"><img src="images/menu-icon/icon16.svg" alt="" class="svg">Header <span>New</span></a></li>
-									                    	</ul>
-									                    </div>
-									                    <div class="col-lg-3 col-md-3 mt-md-3 mb-md-3 mt-lg-3 mb-lg-3">
-									                    	<h6 class="mega-menu-title">Element 2</h6>
-									                    	<ul class="list-unstyled mega-dropdown-list">
-										                        <li><a href="ui-accordion.html"><img src="images/menu-icon/icon7.svg" alt="" class="svg">Accordion</a></li>
-										                        <li><a href="ui-alert.html"><img src="images/menu-icon/icon8.svg" alt="" class="svg">Alert</a></li>
-										                        <li><a href="ui-badges.html"><img src="images/menu-icon/icon9.svg" alt="" class="svg">Badges</a></li>
-										                        <li><a href="ui-gallery.html"><img src="images/menu-icon/icon10.svg" alt="" class="svg">Gallery</a></li>
-										                        <li><a href="ui-typography.html"><img src="images/menu-icon/icon11.svg" alt="" class="svg">Typography</a></li>
-										                        <li><a href="ui-tabs.html"><img src="images/menu-icon/icon18.svg" alt="" class="svg">Tabs</a></li>
-									                    	</ul>								                    </div>
-									                    <div class="col-lg-3 col-md-3 mt-md-3 mb-md-3 mt-lg-3 mb-lg-3">
-									                    	<h6 class="mega-menu-title">Element 3</h6>
-									                    	<ul class="list-unstyled mega-dropdown-list">
-										                        <li><a href="ui-blog-post.html"><img src="images/menu-icon/icon12.svg" alt="" class="svg">Post</a></li>
-										                        <li><a href="ui-breadcrumbs.html"><img src="images/menu-icon/icon13.svg" alt="" class="svg">Breadcrumbs</a></li>
-										                        <li><a href="ui-button.html"><img src="images/menu-icon/icon14.svg" alt="" class="svg">Button</a></li>
-										                        <li><a href="ui-list.html"><img src="images/menu-icon/icon15.svg" alt="" class="svg">List</a></li>
-										                        <li><a href="ui-footer.html"><img src="images/menu-icon/icon16.svg" alt="" class="svg">Footer</a></li>
-										                        <li><a href="ui-subscribe.html"><img src="images/menu-icon/icon4.svg" alt="" class="svg">Newsletter </a></li>
-									                    	</ul>
-									                    </div>
-									                    <div class="col-lg-3 col-md-3 mt-md-3 mb-md-3 mt-lg-3 mb-lg-3">
-									                    	<h6 class="mega-menu-title">Element 4</h6>
-									                    	<ul class="list-unstyled mega-dropdown-list">
-										                        <li><a href="ui-color.html"><img src="images/menu-icon/icon17.svg" alt="" class="svg">Color</a></li>
-										                        <li><a href="ui-contact-form.html"><img src="images/menu-icon/icon18.svg" alt="" class="svg">Contact Form</a></li>
-										                        <li><a href="ui-counter.html"><img src="images/menu-icon/icon19.svg" alt="" class="svg">Counter</a></li>
-										                        <li><a href="ui-pagination.html"><img src="images/menu-icon/icon20.svg" alt="" class="svg">Pagination</a></li>
-										                        <li><a href="ui-modal.html"><i class="icon flaticon-login"></i> Modal</a></li>
-										                        <li><a href="ui-client.html"><img src="images/menu-icon/icon3.svg" alt="" class="svg">Client</a></li>
-									                    	</ul>
-									                    </div>
-								                	</div>
-								              	</div> <!-- /.container -->
-								            </li>
-							        	</ul>
-							        </li>
+								   
 								    <li class="nav-item dropdown">
 							            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">News.</a>
 							            <ul class="dropdown-menu">
@@ -306,7 +113,7 @@
 									        <li><a href="blog-details-v1.html" class="dropdown-item">blog details full grid</a></li>
 							            </ul> 
 							        </li>
-							   </ul>
+							   </ul> -->
 						   </div>
 						</div> 
 					</nav> 
