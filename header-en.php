da55ad42bd96b393eccca0fc9d1bb9160591f52c
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-06-16 09:30:48 +0300

    Adding header arabic & English

---
+++ header-en.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="keywords" content="Digital marketing agency, Digital marketing company, Digital marketing services">
+		<meta name="author" content="creativegigs">
+		<meta name="description" content="rogan creative multipurpose is a beautiful website template designed for SEO & Digital Agency websites.">
+		<meta name='og:image' content='images/home/ogg.png'>
+		<!-- For IE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<!-- For Resposive Device -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- For Window Tab Color -->
+		<!-- Chrome, Firefox OS and Opera -->
+		<meta name="theme-color" content="#fff">
+		<!-- Windows Phone -->
+		<meta name="msapplication-navbutton-color" content="#fff">
+		<!-- iOS Safari -->
+		<meta name="apple-mobile-web-app-status-bar-style" content="#fff">
+		<title>Tamer Chair For Pharmaceutical Industries </title>
+		<!-- Favicon -->
+		<link rel="icon" type="image/png" sizes="56x56" href="<?php bloginfo('template_directory')?>/images/fav-icon/chair-icon.png">
+
+	 
