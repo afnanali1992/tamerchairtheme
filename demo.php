828779b30478d055ec85930b37c29f2b87393594
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-06-20 14:05:25 +0300

    About us interational

---
+++ demo.php
@@ -0,0 +1,36 @@
+<div class="about-us-block-two pt-250 pb-150 md-pt-100 md-pb-100 pos-r">
+				<div class="shape-wrapper">
+
+				</div> <!-- /.shape-wrapper -->
+				<div class="inner-wrapper pos-r">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-6 order-lg-last">
+								<div class="theme-title-one md-p0">
+									<div class="upper-title mb-5">Goals</div>
+									<h2 class="main-title">The Chair's <i>Goals.</i></h2>
+								</div> <!-- /.theme-title-one -->
+								
+								<ul class="theme-list-item pb-70">
+								<li><span><b>1.</b></span> Contributing to the realization of the Kingdom's 2030 vision, especially in the "prosperous economy", by developing and localizing the pharmaceutical industries.
+</li>
+								<li><span><b>2.</b></span> Promote partnership and integration between the government sector (represented by universities and various research centers) and the private sector (represented by research centers, pharmaceutical companies and factories) to develop and localize pharmaceutical industries in accordance with the goals of Vision 2030.
+</li>
+								<li><span><b>3.</b></span> Participating in bringing about a major paradigm shift in the pharmaceutical research system at the university to serve the development and localization of pharmaceutical industries.
+</li>
+								<li><span><b>4.</b></span> To make the chair become a distinguished research body in drug research and the pharmaceutical industries, and to be one of the best scientific and practical references at the local and international level.
+</li>
+							
+							</ul>
+							</div>
+							<div class="col-lg-6 order-lg-first">
+								<div class="main-img-box pos-r hide-pr md-mt-50">
+									<div class="img-one pos-r wow fadeInLeft" data-wow-duration="2s"><img src=/wp-content/themes/Afnan/images/home/home-1.jpeg" alt="home-1" style="height: 100%;"></div>
+									<div class="box-shape pos-r wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.2s"></div>
+									<div class="dot-shape pos-r wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.2s"><img src="/wp-content/themes/Afnan/images/shape/dot-shape.svg" alt=""></div>
+								</div>
+							</div>
+						</div>
+					</div> <!-- /.container -->
+				</div> <!-- /.inner-wrapper -->
+			</div> <!-- /.about-us-block-two -->
