33f0f8b3a209e30a4476c0ae782f375add000214
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-06-23 12:24:10 +0300

    Arabic About Style Css

---
+++ demo2.php
@@ -0,0 +1,33 @@
+<!-- 
+			=============================================
+				Banner Large Text
+			============================================== 
+			-->
+<div class="banner-large-text pt-250 pb-350 pos-r md-pt-200 md-pb-150">
+    <div class="shape-wrapper">
+        <img src="/wp-content/themes/Afnan/images/shape/13.svg" alt="" class="shape-two img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/5.svg" alt="" class="shape-three img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/4.svg" alt="" class="shape-four img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/47.svg" alt="" class="shape-five img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/4.svg" alt="" class="shape-six img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/48.svg" alt="" class="shape-seven img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/9.svg" alt="" class="shape-eight img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/49.svg" alt="" class="shape-nine img-shape">
+    </div>
+    <div class="container">
+        <div class="inner-wrapper pos-r">
+            <div class="shape-wrapper">
+                <div class="img-shape shape-one"><img style="filter: contrast(40%);"
+                        src="/wp-content/themes/Afnan/images/home/home-4.jpeg" alt=""></div>
+            </div>
+            <h2 class="title"><span class="pos-r">لمحة عن </span> <br> كرسي محمد <br> سعيد تمر.</h2>
+            <div class="row">
+                <div class="col-xl-5 col-lg-6 col-md-7">
+                    <p class="sub-heading pb-25">The best award winning digital agency</p>
+                    <p>We put your ideas and thus your wishes in the form of a unique web project that inspires you and
+                        you customers. We’ve team to solve your problems.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- /.banner-large-text -->
