1088692c1cd3c7600478edda2bad68a6209e93e1
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-06-23 13:08:24 +0300

    Arabic About Style Css

--- demo2.php
+++ demo2.php
@@ -1,33 +1,97 @@
-<!-- 
-			=============================================
-				Banner Large Text
-			============================================== 
-			-->
-<div class="banner-large-text pt-250 pb-350 pos-r md-pt-200 md-pb-150">
+<div class="about-us-block-two pt-250 pb-150 md-pt-100 md-pb-100 pos-r">
     <div class="shape-wrapper">
-        <img src="/wp-content/themes/Afnan/images/shape/13.svg" alt="" class="shape-two img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/5.svg" alt="" class="shape-three img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/4.svg" alt="" class="shape-four img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/47.svg" alt="" class="shape-five img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/4.svg" alt="" class="shape-six img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/48.svg" alt="" class="shape-seven img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/9.svg" alt="" class="shape-eight img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/49.svg" alt="" class="shape-nine img-shape">
-    </div>
-    <div class="container">
-        <div class="inner-wrapper pos-r">
-            <div class="shape-wrapper">
-                <div class="img-shape shape-one"><img style="filter: contrast(40%);"
-                        src="/wp-content/themes/Afnan/images/home/home-4.jpeg" alt=""></div>
-            </div>
-            <h2 class="title"><span class="pos-r">لمحة عن </span> <br> كرسي محمد <br> سعيد تمر.</h2>
+
+    </div> <!-- /.shape-wrapper -->
+    <div class="inner-wrapper pos-r">
+        <div class="container">
             <div class="row">
-                <div class="col-xl-5 col-lg-6 col-md-7">
-                    <p class="sub-heading pb-25">The best award winning digital agency</p>
-                    <p>We put your ideas and thus your wishes in the form of a unique web project that inspires you and
-                        you customers. We’ve team to solve your problems.</p>
+                <div class="col-lg-6 order-lg-first">
+                    <div class="theme-title-one md-p0">
+                        <div class="upper-title mb-5">الأهداف</div>
+                        <h2 class="main-title">.أهداف الكرسي</h2>
+                    </div> <!-- /.theme-title-one -->
+
+                    <ul class="theme-list-item pb-70">
+                        <li><span><b>1.</b></span> المساهمة في تحقيق رؤية المملكة 2030، وبخاصة في محور "اقتصاد مزدهر" وذلك بتطوير وتوطين الصناعات الدوائية.
+
+                        </li>
+                        <li><span><b>2.</b></span> تعزيز الشراكة والتكامل بين القطاع الحكومي (المتمثل في الجامعات والمراكز البحثية المختلفة) والقطاع الخاص(المتمثل في مراكز البحوث وشركات ومصانع الأدوية) لتطوير وتوطين الصناعات الدوائية وفق أهداف رؤية 2030.
+
+                        </li>
+                        <li><span><b>3.</b></span> المشاركة في إحداث نقلة نوعية كبيرة في المنظومة البحثية الدوائية في الجامعة بما يخدم تطوير وتوطين الصناعات الدوائية.
+
+                        </li>
+                        <li><span><b>4.</b></span> أن يصبح الكرسي جهة بحثية متميزة في بحوث الدواء والصناعات الدوائية ومن أفضل المراجع العلمية والعملية على المستوى المحلي والدولي.
+
+                        </li>
+
+                    </ul>
+                </div>
+                <div class="col-lg-6 order-lg-last">
+                    <div class="main-img-box pos-r hide-pr md-mt-50">
+                        <div class="img-one pos-r wow fadeInRight" data-wow-duration="2s"><img
+                                src=/wp-content/themes/Afnan/images/home/home-1.jpeg" alt="home-1"
+                                style="height: 100%;"></div>
+                        <div class="box-shape pos-r wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.2s"></div>
+                        <div class="dot-shape pos-r wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.2s"><img
+                                src="/wp-content/themes/Afnan/images/shape/dot-shape.svg" alt=""></div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div> <!-- /.banner-large-text -->
+        </div> <!-- /.container -->
+    </div> <!-- /.inner-wrapper -->
+</div> <!-- /.about-us-block-two -->
+
+
+<div class="about-us-block-two pt-250 pb-150 md-pt-100 md-pb-100 pos-r">
+    <div class="shape-wrapper">
+
+    </div> <!-- /.shape-wrapper -->
+    <div class="inner-wrapper pos-r">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 order-lg-last" style="text-align: right;">
+                    <div class="theme-title-one md-p0">
+                        <div class="upper-title mb-5">الخطة</div>
+                        <h2 class="main-title">خطة الكرسي . </h2>
+                        <!-- <div class="mark-text pt-25 pb-35">related to the chair (universities, institutes, centers,
+                            scientific chairs).</div> -->
+                    </div> <!-- /.theme-title-one -->
+
+                    <ul class="theme-list-item pb-70">
+                        <li><span><b>1.</b></span> تقديم البحوث والدراسات العلمية المتميزة من اكتشاف الدواء إلى تطويره وتصنيعه.
+
+                        </li>
+                        <li><span><b>2.</b></span> تقديم وتطوير البحوث وبراءات الاختراع في المجال الدوائي وتحويلها إلى منتجات دوائية تصنع محلياً بالتعاون مع الجامعات ومراكز البحوث وشركات ومصانع الأدوية المحلية والدولية.
+                        <li><span><b>3.</b></span>تقديم الخدمات البحثية العلمية والعملية في مجال تطوير وتوطين الصناعات الدوائية.
+
+                        </li>
+                        <li><span><b>4.</b></span>تقديم الاستشارات والتوصيات المتعلقة بالصناعات الدوائية.
+
+                        </li>
+                        <li><span><b>5.</b></span>تطوير المبادرات المتعلقة بالصناعات الدوائية. 
+                        </li>
+                        <li><span><b>6.</b></span>إقامة الدورات وعقد ورش العمل لاكتشاف الدواء وتطويره وتصنيعه.
+                        </li>
+                        <li><span><b>7.</b></span>إقامة اللقاءات والمؤتمرات المتميزة في الصناعات الدوائية.
+                        </li>
+                        <li><span><b>8.</b></span>المشاركة في اللقاءات والمؤتمرات المحلية والدولية المتعلقة ببحوث الدواء والصناعات الدوائية.
+                        </li>
+                        <li><span><b>9.</b></span>المساهمة في رسم الخطط الاستراتيجية التي تساهم في تطوير وتوطين الصناعات الدوائية.
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 order-lg-first">
+                    <div class="main-img-box pos-r hide-pr md-mt-50">
+                        <div class="img-one pos-r wow fadeInLeft" data-wow-duration="2s"><img
+                                src=/wp-content/themes/Afnan/images/home/home-3.jpeg" alt="home-1"
+                                style="height: 100%;"></div>
+                        <div class="box-shape pos-r wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.2s"></div>
+                        <div class="dot-shape pos-r wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.2s"><img
+                                src="/wp-content/themes/Afnan/images/shape/dot-shape.svg" alt=""></div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- /.container -->
+    </div> <!-- /.inner-wrapper -->
+</div> <!-- /.about-us-block-two -->
