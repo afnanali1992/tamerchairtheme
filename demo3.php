2a26acf59981ba0cf8338fefd9cdb7cd6e3d9122
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-06-24 09:40:20 +0300

    Staff 9

---
+++ demo3.php
@@ -0,0 +1,126 @@
+<div class="team-minimal our-team pb-70 md-pb-60">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-4 col-md-6">
+							<div class="single-team-member">
+								<div class="wrapper">
+									<div class="img-box">
+										<img src="images/team/10.jpg" alt="">
+										<div class="hover-content">
+											<ul>
+												<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+												<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+												<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+											</ul>
+										</div> <!-- /.hover-content -->
+									</div>
+									<div class="info-meta">
+										<h6 class="name">Hunter Chapman</h6>
+										<span>Co-Founder</span>
+									</div>
+								</div>
+							</div> <!-- /.single-team-member -->
+						</div> <!-- /.col- -->
+						<div class="col-lg-4 col-md-6">
+							<div class="single-team-member">
+								<div class="wrapper">
+									<div class="img-box">
+										<img src="images/team/11.jpg" alt="">
+										<div class="hover-content">
+											<ul>
+												<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+												<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+												<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+											</ul>
+										</div> <!-- /.hover-content -->
+									</div>
+									<div class="info-meta">
+										<h6 class="name">Richard Lowe</h6>
+										<span>UI/UX designer</span>
+									</div>
+								</div>
+							</div> <!-- /.single-team-member -->
+						</div> <!-- /.col- -->
+						<div class="col-lg-4 col-md-6">
+							<div class="single-team-member">
+								<div class="wrapper">
+									<div class="img-box">
+										<img src="images/team/12.jpg" alt="">
+										<div class="hover-content">
+											<ul>
+												<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+												<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+												<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+											</ul>
+										</div> <!-- /.hover-content -->
+									</div>
+									<div class="info-meta">
+										<h6 class="name">Johnny Yates</h6>
+										<span>Project Manager</span>
+									</div>
+								</div>
+							</div> <!-- /.single-team-member -->
+						</div> <!-- /.col- -->
+						<div class="col-lg-4 col-md-6">
+							<div class="single-team-member">
+								<div class="wrapper">
+									<div class="img-box">
+										<img src="images/team/13.jpg" alt="">
+										<div class="hover-content">
+											<ul>
+												<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+												<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+												<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+											</ul>
+										</div> <!-- /.hover-content -->
+									</div>
+									<div class="info-meta">
+										<h6 class="name">Ella Saunders</h6>
+										<span>SEO Expert</span>
+									</div>
+								</div>
+							</div> <!-- /.single-team-member -->
+						</div> <!-- /.col- -->
+						<div class="col-lg-4 col-md-6">
+							<div class="single-team-member">
+								<div class="wrapper">
+									<div class="img-box">
+										<img src="images/team/14.jpg" alt="">
+										<div class="hover-content">
+											<ul>
+												<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+												<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+												<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+											</ul>
+										</div> <!-- /.hover-content -->
+									</div>
+									<div class="info-meta">
+										<h6 class="name">Jerry Buchanan</h6>
+										<span>Developer</span>
+									</div>
+								</div>
+							</div> <!-- /.single-team-member -->
+						</div> <!-- /.col- -->
+						<div class="col-lg-4 col-md-6">
+							<div class="single-team-member">
+								<div class="wrapper">
+									<div class="img-box">
+										<img src="images/team/15.jpg" alt="">
+										<div class="hover-content">
+											<ul>
+												<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+												<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+												<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+											</ul>
+										</div> <!-- /.hover-content -->
+									</div>
+									<div class="info-meta">
+										<h6 class="name">Joshua Parsons</h6>
+										<span>Marketing Expert</span>
+									</div>
+								</div>
+							</div> <!-- /.single-team-member -->
+						</div> <!-- /.col- -->
+					</div> <!-- /.row -->
+				</div> <!-- /.container -->
+			</div> <!-- /.team-minimal -->
