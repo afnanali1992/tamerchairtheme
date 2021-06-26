41569a8f9ab4f1abe1dc7a9dea4b8ea141606cf0
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-06-26 12:42:45 +0300

    Header

--- header.php
+++ header.php
@@ -6,24 +6,24 @@ elseif( pll_current_language() == 'ar' ):
 endif
 ?>
 
-		<?php wp_head(); ?>
+<?php wp_head(); ?>
 
-		<!-- Fix Internet Explorer ______________________________________-->
-		<!--[if lt IE 9]>
+<!-- Fix Internet Explorer ______________________________________-->
+<!--[if lt IE 9]>
 			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 			<script src="vendor/html5shiv.js"></script>
 			<script src="vendor/respond.js"></script>
-		<![endif]-->	
-	</head>
+		<![endif]-->
+</head>
 
-	<body>
-		<div class="main-page-wrapper">
+<body>
+	<div class="main-page-wrapper">
 
-			<!-- ===================================================
+		<!-- ===================================================
 				Loading Transition
 			==================================================== -->
-			<!-- Preloader -->
-			<!-- <section>
+		<!-- Preloader -->
+		<!-- <section>
 				<div id="preloader">
 					<div id="ctn-preloader" class="ctn-preloader">
 						<div class="animation-preloader">
@@ -51,40 +51,42 @@ endif
 			</section> -->
 
 
-			<!-- 
+		<!-- 
 			=============================================
 				Theme Main Menu
 			============================================== 
 			-->
-            <header>
+		<header>
 			<div class="theme-main-menu theme-menu-two main-p-color">
-         
-			<div class="d-flex  logo-header row align-items-top justify-content-between">
-				<!-- LOGO -->
-				<div class="logo lg-3"><a href="index.html"><img src="<?php bloginfo('template_directory')?>/images/logo/tamer-logo-3.png" alt=""></a></div>
-				
-					
+
+				<div class="d-flex  logo-header row align-items-top justify-content-between">
+					<!-- LOGO -->
+					<div class="logo lg-3" style="margin-top: -18px;"><a href="index.html"><img src="<?php bloginfo('template_directory')?>/images/logo/tamer-logo-1.png" alt=""></a></div>
+
+
 					<!-- <div class="logo lg-3"><a href="index.html"><img src="<?php bloginfo('template_directory')?>/images/logo/tamer-logo-2.png" alt=""></a></div> -->
 					<!-- <div class="right-content ml-auto order-lg-3">
 						<a href="contact-v1.html" class="theme-btn line-button-one button-rose contact-button button-white-bg">Contact us</a>
 					</div> -->
-		 
-		
-                    
-     
-           
-			 
-				<!-- LOGO -->
+
+
+
+
+
+
+					<!-- LOGO -->
 					<!-- <div class="logo"><a href="index.html"><img src="<?php bloginfo('template_directory')?>/images/logo/logo.svg" alt=""></a></div> -->
 
 
-					<nav id="mega-menu-holder" class="navbar navbar-expand-lg order-lg-2">
-						<div  class="container nav-container">
-							<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						        <i class="flaticon-setup"></i>
-						    </button>
-						   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-						   		<?php wp_nav_menu(
+					<nav id="mega-menu-holder" class="navbar navbar-expand-lg lg-2">
+						<div class="container nav-container">
+							<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+								data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+								aria-expanded="false" aria-label="Toggle navigation">
+								<i class="flaticon-setup"></i>
+							</button>
+							<div class="collapse navbar-collapse" id="navbarSupportedContent">
+								<?php wp_nav_menu(
 									   array(
 										   'theme_location' => 'header',
 										   'container'=> 'ul',
@@ -93,8 +95,8 @@ endif
 										   
 									   )
 									   ); ?>
-								   
-								   <!-- <ul class="navbar-nav">
+
+								<!-- <ul class="navbar-nav">
 								    <li class="nav-item dropdown mega-dropdown-holder active">
 							            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Home</a>
 							        </li>
@@ -112,18 +114,21 @@ endif
 							            </ul> 
 							        </li>
 							   </ul> -->
-						   </div>
-						</div> 
-					</nav> 
-		 
-					<div class="right-content  order-lg-3">
-						<a href="contact-v1.html" class="theme-btn line-button-one button-rose contact-button button-white-bg">Contact us</a>
+							</div>
+						</div>
+					</nav>
+
+					<div class="lg-3">
+						<a href="contact-v1.html"
+							class="theme-btn line-button-one button-rose contact-button button-white-bg">Contact us</a>
 					</div>
 
-				<div class="logo lg-3"><a href="index.html"><img src="<?php bloginfo('template_directory')?>/images/logo/tamer-logo-1.png" alt=""></a></div>
+					<div class="logo lg-3" style="margin-bottom: -12px;"><a href="index.html"><img
+								src="<?php bloginfo('template_directory')?>/images/logo/tamer-logo-3.png" alt=""></a>
+					</div>
 
 				</div>
-		 
+
 			</div> <!-- /.theme-main-menu -->
 
-            </header>
+		</header>
