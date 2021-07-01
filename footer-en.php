6ecdda2e146b6cb200d35f6571225ae3ea16f208
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-07-01 21:40:11 +0300

    Footer ar ,en

---
+++ footer-en.php
@@ -0,0 +1,69 @@
+<footer class="theme-footer-one main-p-color pt-130 newsletter-two">
+				<div class="top-footer">
+					<div class="container text-xs-center">
+						<div class="row">
+							<div class="col-lg-3 col-sm-6 col-12 footer-about-widget">
+								<a href="index.html" class="logo"><img src="images/logo/logo.svg" alt=""></a>
+								<a href="#" class="email">info@tamerchair.org</a>
+								<a href="#" class="phone">720.661.2234</a>
+								
+							</div> <!-- /.about-widget -->
+							<div class="col-lg-3 col-lg-3 col-sm-6 col-12 footer-list xs-hide">
+								<h5 class="footer-title">Links</h5>
+								<ul>
+									<li><a href="#">Home</a></li>
+									<li><a href="#">Awards</a></li>
+									<li><a href="#">Publications</a></li>
+									<li><a href="#">Achievements</a></li>
+									<!-- <li><a href="#">Contact </a></li> -->
+								</ul>
+							</div> <!-- /.footer-recent-post -->
+							<div class="col-lg-3 col-sm-6 col-12 footer-list xs-hide">
+								<h5 class="footer-title">About us</h5>
+								<ul>
+									<li><a href="about-standard.html">About us</a></li>
+									<li><a href="team-v1.html">Team</a></li>
+									<li><a href="blog-v1.html">News</a></li>
+								</ul>
+							</div> <!-- /.footer-list -->
+							<div class="col-lg-3 col-lg-2 col-sm-6 col-12 footer-information">
+								 <h5 class="footer-title">Our Address</h5>
+								<p>Faculty of Pharmacy <br> King Abdulaziz University <br> Jeddah, Saudi Arabia <br> Postal Code: 21589 - P.O.Box: 80200</p> 
+								<!-- <p>Follow us to get the latest news from Tamer Chair</p> -->
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div> <!-- /.row -->
+					</div> <!-- /.container -->
+				</div> <!-- /.top-footer -->
+				
+				<div class="container">
+					<div class="bottom-footer-content">
+						<p>&copy; <?php echo date("Y");?> All rights reserved for Saudi Scientific Society of Pharmaceutical Industries. <br>Trade marks are owned by or licensed to the King Abdulaziz University.</p>
+					</div> <!-- /.bottom-footer -->
+				</div>
+			</footer> <!-- /.theme-footer-one -->
+			
+
+	        
+
+	        <!-- Scroll Top Button -->
+			<button class="scroll-top tran3s">
+				<i class="fa fa-angle-up" aria-hidden="true"></i>
+			</button>
+
+			
+
+
+		<!-- Optional JavaScript _____________________________  -->
+
+    	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    	<!-- jQuery -->
+	<?php wp_footer();?>
+		</div> <!-- /.main-page-wrapper -->
+	</body>
+</html>
