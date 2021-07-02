101ca76c2a299a450c3e536bd51e8ef92e89c245
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-07-03 00:50:13 +0300

    Staff Space

--- demo.php
+++ demo.php
@@ -1,160 +1,101 @@
-<div class="about-us-text bg-color pos-r pb-200 md-pt-50 md-pb-50">
+<div class="pricing-plan-classic pt-350 pb-30 md-pt-130 d-xl-none	 pos-r">
+    <div class="shape-wrapper">
+        <svg class="img-shape shape-one">
+            <path fill-rule="evenodd" fill="rgb(201, 192, 172)"
+                d="M7.500,15.000 C11.642,15.000 15.000,11.641 15.000,7.499 C15.000,3.357 11.642,-0.000 7.500,-0.000 C3.358,-0.000 -0.000,3.357 -0.000,7.499 C-0.000,11.641 3.358,15.000 7.500,15.000 Z">
+            </path>
+        </svg>
+        <svg class="img-shape shape-two">
+            <path fill-rule="evenodd" fill="rgb(244, 235, 205)"
+                d="M6.000,12.000 C9.314,12.000 12.000,9.313 12.000,6.000 C12.000,2.686 9.314,-0.000 6.000,-0.000 C2.686,-0.000 -0.000,2.686 -0.000,6.000 C-0.000,9.313 2.686,12.000 6.000,12.000 Z">
+            </path>
+        </svg>
+        <svg class="img-shape shape-three">
+            <path fill-rule="evenodd" fill="rgb(224, 133, 133)"
+                d="M4.000,8.000 C6.209,8.000 8.000,6.208 8.000,3.999 C8.000,1.790 6.209,-0.001 4.000,-0.001 C1.791,-0.001 -0.000,1.790 -0.000,3.999 C-0.000,6.208 1.791,8.000 4.000,8.000 Z">
+            </path>
+        </svg>
+        <svg class="img-shape shape-four">
+            <path fill-rule="evenodd" fill="rgb(237, 222, 171)"
+                d="M10.000,19.999 C15.523,19.999 20.000,15.522 20.000,10.000 C20.000,4.476 15.523,-0.000 10.000,-0.000 C4.477,-0.000 -0.000,4.476 -0.000,10.000 C-0.000,15.522 4.477,19.999 10.000,19.999 Z">
+            </path>
+        </svg>
+    </div> <!-- /.shape-wrapper -->
     <div class="container">
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-5 col-md-6 col-12 pt-120 sm-pt-60 sm-pb-0">
                 <div class="theme-title-one">
-                    <div class="upper-title mb-5">Vision & Message</div>
-                    <h2 class="main-title">Tamer Chair.</h2>
+                    <div class="upper-title mb-5">Professor</div>
+                    <h2 class="main-title">Prof. Osama A. Ali Ahmed</h2>
                 </div> <!-- /.theme-title-one -->
-            </div>
-            <div class="col-lg-4 about-text-one">
-                <h4 class="pb-20 md-pt-50">Our Vision </h4>
-                <p>To become a world leader in drug research, development and the localization of the pharmaceutical
-                    industry.</p>
+                <p class="pt-20 sm-pb-0 pb-70">Consultant of pharmaceutical industries and professor of Pharmaceutics and industrial pharmacy, Faculty of Pharmacy, King Abdul-Aziz University.</p>
+            </div> <!-- /.col- -->
+            <div class="col-lg-6 col-md-6 col-12 ml-auto">
+                <div class="tab-content pos-r" data-aos="fade-left" data-aos-duration="2000">
+                    <div class="table-content">
+                        <div id="monthly" class="tab-pane price-table in active">
+                            <div class="col-inner">
+                                <img src="/wp-content/themes/Afnan/images/home/osama.png" alt="">
 
-            </div>
-            <div class="col-lg-4 about-text-one">
-                <h4 class="pb-20 md-pt-50">Our Message</h4>
-                <p>To provide research, patents, products, services and consultancy in the development and localization
-                    of the pharmaceutical industry.
-                </p>
 
+                            </div>
+                        </div> <!-- /#monthly -->
+                        <div id="yearly" class="tab-pane price-table">
+                            <div class="col-inner" style="height:555px; background:">
+                                <!-- <h6>Subway</h6>
+				                            <div class="price pt-25 pb-35">$259.<sup>99</sup></div>
+				                            <div class="tag">Quick job, Small Work</div>
+				                            <ul class="pt-20 pb-40">
+				                               	<li>50GB Bandwidth</li>
+				                                <li>Business &amp; Financ Analysing</li>
+				                                <li>24 hour support</li>
+				                                <li>Customer Managemet</li>
+				                            </ul> -->
+                                <!-- <img src="/wp-content/themes/Afnan/images/home/nabil.png" alt=""> -->
+                            </div>
+                        </div> <!-- /#yearly -->
+                    </div> <!-- /.table-content -->
+                </div> <!-- /.tab-content -->
             </div>
         </div> <!-- /.row -->
+    </div> <!-- /.container -->
+</div> <!-- /.pricing-plan-classic -->
 
 
-    </div>
-</div> <!-- /.container -->
-</div> <!-- /.about-us-text -->
-
-
-<div class="about-us-block-one pt-100 pb-200 md-pt-50 md-pb-50 pos-r">
-    <div class="shape-wrapper">
-        <img src="/wp-content/themes/Afnan/images/shape/21.svg" alt="" class="shape-one img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/23.svg" alt="" class="shape-two img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/24.svg" alt="" class="shape-three img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/17.svg" alt="" class="shape-four img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/9.svg" alt="" class="shape-five img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/25.svg" alt="" class="shape-six img-shape">
-    </div>
-
-</div> <!-- /.shape-wrapper -->
-<div class="about-us-block-two">
-    <div class="inner-wrapper pos-r">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 order-lg-first">
-                    <div class="theme-title-one md-p0">
-                        <div class="upper-title mb-5">Goals</div>
-                        <h2 class="main-title">The Chair's <i>Goals.</i></h2>
-                    </div> <!-- /.theme-title-one -->
-
-                    <ul class="theme-list-item pb-70">
-                        <li><span><b>1.</b></span> Contributing to the realization of the Kingdom's 2030 vision,
-                            especially in the "prosperous economy", by developing and localizing the pharmaceutical
-                            industries.
-                        </li>
-                        <li><span><b>2.</b></span> Promote partnership and integration between the government sector
-                            (represented by universities and various research centers) and the private sector
-                            (represented by research centers, pharmaceutical companies and factories) to develop and
-                            localize pharmaceutical industries in accordance with the goals of Vision 2030.
-                        </li>
-                        <li><span><b>3.</b></span> Participating in bringing about a major paradigm shift in the
-                            pharmaceutical research system at the university to serve the development and localization
-                            of pharmaceutical industries.
-                        </li>
-                        <li><span><b>4.</b></span> To make the chair become a distinguished research body in drug
-                            research and the pharmaceutical industries, and to be one of the best scientific and
-                            practical references at the local and international level.
-                        </li>
-
-                    </ul>
-                </div>
-                <div class="col-lg-6 order-lg-last">
-                    <div class="main-img-box pos-r hide-pr md-mt-50">
-                        <div class="img-one pos-r wow fadeInRight" data-wow-duration="2s"><img
-                                src=/wp-content/themes/Afnan/images/home/home-1.jpeg" alt="home-1"
-                                style="height: 100%;"></div>
-                        <div class="box-shape pos-r wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.2s"></div>
-                        <div class="dot-shape pos-r wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.2s"><img
-                                src="/wp-content/themes/Afnan/images/shape/dot-shape.svg" alt=""></div>
-                    </div>
-                </div>
-            </div>
-        </div> <!-- /.container -->
-    </div> <!-- /.inner-wrapper -->
-</div> <!-- /.about-us-block-two -->
-</div>
-
-<div class="our-core-feature style-one pt-250 pb-150 md-pt-100 md-pb-100 pos-r">
-    <div class="shape-wrapper">
-        <img src="/wp-content/themes/Afnan/images/shape/14.svg" alt="" class="shape-one img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/4.svg" alt="" class="shape-two img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/5.svg" alt="" class="shape-three img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/17.svg" alt="" class="shape-four img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/18.svg" alt="" class="shape-five img-shape">
-        <img src="/wp-content/themes/Afnan/images/shape/19.svg" alt="" class="shape-six img-shape">
-    </div>
-    <div class="about-us-block-two">
-        <div class="inner-wrapper pos-r">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 order-lg-last">
-                        <div class="theme-title-one md-p0">
-                            <div class="upper-title mb-5">Plan</div>
-                            <h2 class="main-title">Chair’s <i> Plan .</i></h2>
-                            <!-- <div class="mark-text pt-25 pb-35">related to the chair (universities, institutes, centers,
-                            scientific chairs).</div> -->
-                        </div> <!-- /.theme-title-one -->
-
-                        <ul class="theme-list-item pb-70">
-                            <li><span><b>1.</b></span> To provide outstanding scientific research and studies from the
-                                discovery of the drug to its development and manufacture.
-                            </li>
-                            <li><span><b>2.</b></span> To generate and develop research and patents in the
-                                pharmaceutical
-                                field and turn them into locally manufactured pharmaceutical products in cooperation
-                                with
-                                universities, research centers, local and international pharmaceutical companies and
-                                factories.
-                            </li>
-                            <li><span><b>3.</b></span>Providing scientific and practical research services in the
-                                development and localization of the pharmaceutical industry.
-                            </li>
-                            <li><span><b>4.</b></span>Providing advice and recommendations related to the pharmaceutical
-                                industry.
-                            </li>
-                            <li><span><b>5.</b></span>Development of initiatives related to the pharmaceutical industry.
-                            </li>
-                            <li><span><b>6.</b></span>Conducting courses related to the discovery, development and
-                                manufacture of drugs.
-                            </li>
-                            <li><span><b>7.</b></span>Holding distinguished meetings and conferences in the
-                                pharmaceutical
-                                industry.
-                            </li>
-                            <li><span><b>8.</b></span>Participating in local and international meetings and conferences
-                                related to pharmaceutical research and pharmaceutical industries.
-                            </li>
-                            <li><span><b>9.</b></span>Contribute to the development of strategic plans that contribute
-                                to
-                                the development and localization of the pharmaceutical industry.
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-6 order-lg-first">
-                        <div class="main-img-box pos-r hide-pr md-mt-50">
-                            <div class="img-one pos-r wow fadeInLeft" data-wow-duration="2s"><img
-                                    src=/wp-content/themes/Afnan/images/home/home-3.jpeg" alt="home-1"
-                                    style="height: 100%;"></div>
-                            <div class="box-shape pos-r wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.2s">
-                            </div>
-                            <div class="dot-shape pos-r wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.2s">
-                                <img src="/wp-content/themes/Afnan/images/shape/dot-shape.svg" alt=""></div>
-                        </div>
-                    </div>
-                </div>
-            </div> <!-- /.container -->
-        </div> <!-- /.inner-wrapper -->
-    </div> <!-- /.about-us-block-two -->
-</div>
+<div class="about-us-block-one pt-350 pb-30 mb-150 md-pt-130 pos-r  d-none d-xl-block">
+				<div class="shape-wrapper">
+					<img src="/wp-content/themes/Afnan/images/shape/21.svg" alt="" class="shape-one img-shape">
+					<img src="/wp-content/themes/Afnan/images/shape/23.svg" alt="" class="shape-two img-shape">
+					<img src="/wp-content/themes/Afnan/images/shape/24.svg" alt="" class="shape-three img-shape">
+					<img src="/wp-content/themes/Afnan/images/shape/17.svg" alt="" class="shape-four img-shape">
+					<img src="/wp-content/themes/Afnan/images/shape/9.svg" alt="" class="shape-five img-shape">
+					<img src="/wp-content/themes/Afnan/images/shape/25.svg" alt="" class="shape-six img-shape">
+				</div> <!-- /.shape-wrapper -->
+				<div class="inner-wrapper pos-r">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-6">
+								<div class="theme-title-one">
+									<div class="upper-title mb-5">Professor</div>
+									<h2 class="main-title underline"><span>Chair’s Professor
+									</span> <span>Prof. Osama A. Ali Ahmed</span></h2>
+								</div> <!-- /.theme-title-one -->
+								<p class="pt-40 pb-45"> Consultant of pharmaceutical industries and professor of Pharmaceutics and industrial pharmacy, Faculty of Pharmacy, King Abdul-Aziz University.</p>
+								<!-- <p class="bold-text pb-60">Lorem ipsum dolor sit amet, hendrerit omittantur melu, est ut vidit animal iracundia luis.</p> -->
+								<!-- <a href="#" class="theme-btn solid-button-one" data-aos="fade-up">Learn More <i class="fa fa-angle-right" aria-hidden="true"></i></a> -->
+							</div>
+						</div>
+					</div> <!-- /.container -->
+					<div class="path-shape-holder">
+						<img src="/wp-content/themes/Afnan/images/shape/16.svg" alt="" data-aos="fade-left">
+						<div class="image-box" data-aos="fade-left" data-aos-delay="300">
+							<svg width="666" height="578">
+								<g clip-path="url(#path-shape-one)">
+									<image width="666" height="578" href="/wp-content/themes/Afnan/images/home/osama.png" class="image-shape">
+									</image>
+								</g>
+							</svg>
+						</div>
+					</div> <!-- /.path-shape-holder -->
+				</div> <!-- /.inner-wrapper -->
+			</div> <!-- /.about-us-block-one -->
