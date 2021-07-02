3724876b2609e6f502f182c0af80e2506f7a84a0
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-07-02 23:57:22 +0300

    Footer

--- demo.php
+++ demo.php
@@ -1,56 +1,82 @@
-<div class="about-us-block-two pt-250 pb-150 md-pt-100 md-pb-100 pos-r">
-    <div class="shape-wrapper">
+<div class="about-us-text bg-color pos-r pb-200 md-pt-50 md-pb-50">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="theme-title-one">
+                    <div class="upper-title mb-5">Vision & Message</div>
+                    <h2 class="main-title">Tamer Chair.</h2>
+                </div> <!-- /.theme-title-one -->
+            </div>
+            <div class="col-lg-4 about-text-one">
+                <h4 class="pb-20 md-pt-50">Our Vision </h4>
+                <p>To become a world leader in drug research, development and the localization of the pharmaceutical
+                    industry.</p>
 
-    </div> <!-- /.shape-wrapper -->
+            </div>
+            <div class="col-lg-4 about-text-one">
+                <h4 class="pb-20 md-pt-50">Our Message</h4>
+                <p>To provide research, patents, products, services and consultancy in the development and localization
+                    of the pharmaceutical industry.
+                </p>
+
+            </div>
+        </div> <!-- /.row -->
+
+
+    </div>
+</div> <!-- /.container -->
+</div> <!-- /.about-us-text -->
+
+
+<div class="about-us-block-one pt-100 pb-200 md-pt-50 md-pb-50 pos-r">
+    <div class="shape-wrapper">
+        <img src="/wp-content/themes/Afnan/images/shape/21.svg" alt="" class="shape-one img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/23.svg" alt="" class="shape-two img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/24.svg" alt="" class="shape-three img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/17.svg" alt="" class="shape-four img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/9.svg" alt="" class="shape-five img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/25.svg" alt="" class="shape-six img-shape">
+    </div>
+
+</div> <!-- /.shape-wrapper -->
+<div class="about-us-block-two">
     <div class="inner-wrapper pos-r">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 order-lg-first">
                     <div class="theme-title-one md-p0">
-                        <div class="upper-title mb-5">Plan</div>
-                        <h2 class="main-title">Chair’s <i> Plan .</i></h2>
-                        <!-- <div class="mark-text pt-25 pb-35">related to the chair (universities, institutes, centers,
-                            scientific chairs).</div> -->
+                        <div class="upper-title mb-5">Goals</div>
+                        <h2 class="main-title">The Chair's <i>Goals.</i></h2>
                     </div> <!-- /.theme-title-one -->
 
                     <ul class="theme-list-item pb-70">
-                        <li><span><b>1.</b></span> To provide outstanding scientific research and studies from the
-                            discovery of the drug to its development and manufacture.
+                        <li><span><b>1.</b></span> Contributing to the realization of the Kingdom's 2030 vision,
+                            especially in the "prosperous economy", by developing and localizing the pharmaceutical
+                            industries.
                         </li>
-                        <li><span><b>2.</b></span> To generate and develop research and patents in the pharmaceutical
-                            field and turn them into locally manufactured pharmaceutical products in cooperation with
-                            universities, research centers, local and international pharmaceutical companies and
-                            factories.
+                        <li><span><b>2.</b></span> Promote partnership and integration between the government sector
+                            (represented by universities and various research centers) and the private sector
+                            (represented by research centers, pharmaceutical companies and factories) to develop and
+                            localize pharmaceutical industries in accordance with the goals of Vision 2030.
                         </li>
-                        <li><span><b>3.</b></span>Providing scientific and practical research services in the
-                            development and localization of the pharmaceutical industry.
+                        <li><span><b>3.</b></span> Participating in bringing about a major paradigm shift in the
+                            pharmaceutical research system at the university to serve the development and localization
+                            of pharmaceutical industries.
                         </li>
-                        <li><span><b>4.</b></span>Providing advice and recommendations related to the pharmaceutical
-                            industry.
+                        <li><span><b>4.</b></span> To make the chair become a distinguished research body in drug
+                            research and the pharmaceutical industries, and to be one of the best scientific and
+                            practical references at the local and international level.
                         </li>
-                        <li><span><b>5.</b></span>Development of initiatives related to the pharmaceutical industry.
-                        </li>
-                        <li><span><b>6.</b></span>Conducting courses related to the discovery, development and
-                            manufacture of drugs.
-                        </li>
-                        <li><span><b>7.</b></span>Holding distinguished meetings and conferences in the pharmaceutical
-                            industry.
-                        </li>
-                        <li><span><b>8.</b></span>Participating in local and international meetings and conferences
-                            related to pharmaceutical research and pharmaceutical industries.
-                        </li>
-                        <li><span><b>9.</b></span>Contribute to the development of strategic plans that contribute to
-                            the development and localization of the pharmaceutical industry.
-                        </li>
+
                     </ul>
                 </div>
                 <div class="col-lg-6 order-lg-last">
                     <div class="main-img-box pos-r hide-pr md-mt-50">
-                        <div class="img-one pos-r wow fadeInLeft" data-wow-duration="2s"><img
-                                src=/wp-content/themes/Afnan/images/home/home-5.jpeg" alt="home-1"
+                        <div class="img-one pos-r wow fadeInRight" data-wow-duration="2s"><img
+                                src=/wp-content/themes/Afnan/images/home/home-1.jpeg" alt="home-1"
                                 style="height: 100%;"></div>
-                        <div class="box-shape pos-r wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.2s"></div>
-                        <div class="dot-shape pos-r wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.2s"><img
+                        <div class="box-shape pos-r wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.2s"></div>
+                        <div class="dot-shape pos-r wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.2s"><img
                                 src="/wp-content/themes/Afnan/images/shape/dot-shape.svg" alt=""></div>
                     </div>
                 </div>
@@ -58,107 +84,77 @@
         </div> <!-- /.container -->
     </div> <!-- /.inner-wrapper -->
 </div> <!-- /.about-us-block-two -->
+</div>
 
-
-
-
-<div class="why-choose-us pos-r pt-250 pb-150">
+<div class="our-core-feature style-one pt-250 pb-150 md-pt-100 md-pb-100 pos-r">
     <div class="shape-wrapper">
-        <span class="big-round-one wow fadeInLeft animated" data-wow-duration="3s"></span>
-        <span class="big-round-two wow fadeInLeft animated" data-wow-duration="3s"></span>
-        <span class="big-round-three wow fadeInLeft animated" data-wow-duration="3s"></span>
-    </div> <!-- /.shape-wrapper -->
-    <div class="container">
-        <div class="clearfix">
-            <div class="text-wrapper">
-                <h2 class="main-title">Tamer <i> Group .</i></h2>
-                <p class="bottom-title">was established in 1922 by Dr. Mohammed Saeed Tamer .</p>
-                <p>
-                    as the first pharmacy in the kingdom of Saudi Arabia, and he began to expand his field of work by
-                    importing and distributing medicines in the Kingdom. In 1963, Dr. Farouk Tamer and Mr. Mamoun Tamer
-                    took over the management and development of the company to occupy the forefront in the fields of
-                    medicine, medical equipment and consumer goods. Since 1985, the Tamer Group has continued to move
-                    forward and lead the third generation of sons to be able to produce medicines locally, and it has
-                    also been able to achieve strategic and important investments in the fields of trade, finance,
-                    insurance and information technology. The group’s activities focus on importing, distributing,
-                    promoting, marketing and manufacturing a wide range of products and services. </p>
-                <div class="director-speech clearfix">
-                    <img src="images/home/7.jpg" alt="" class="d-img">
-                    <div class="bio-block">
-                        <h3 class="main-title">Tamer Group's <i> Mission .</i></h3>
-                        <p>: to achieve leadership at the regional level in the fields of medicines, health care,
-                            nutrition, health, beauty and well-being to meet the needs of society.</p>
-                    </div> <!-- /.bio-block -->
-                    <!-- <img src="images/home/sign.png" alt="" class="sign"> -->
-                </div> <!-- /.director-speech -->
-            </div> <!-- /.text-wrapper -->
-        </div>
-    </div> <!-- /.container -->
-    <div class="screen-preview">
-        <img src="/wp-content/themes/Afnan/images/home/home-3.jpeg" alt="">
+        <img src="/wp-content/themes/Afnan/images/shape/14.svg" alt="" class="shape-one img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/4.svg" alt="" class="shape-two img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/5.svg" alt="" class="shape-three img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/17.svg" alt="" class="shape-four img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/18.svg" alt="" class="shape-five img-shape">
+        <img src="/wp-content/themes/Afnan/images/shape/19.svg" alt="" class="shape-six img-shape">
     </div>
-</div> <!-- /.why-choose-us -->
+    <div class="about-us-block-two">
+        <div class="inner-wrapper pos-r">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 order-lg-last">
+                        <div class="theme-title-one md-p0">
+                            <div class="upper-title mb-5">Plan</div>
+                            <h2 class="main-title">Chair’s <i> Plan .</i></h2>
+                            <!-- <div class="mark-text pt-25 pb-35">related to the chair (universities, institutes, centers,
+                            scientific chairs).</div> -->
+                        </div> <!-- /.theme-title-one -->
 
-
-<div class="testimonial-section-classic style-one pos-r md-pt-40 md-pb-200">
-    <img src="/wp-content/themes/Afnan/images/shape/33.svg" alt="" class="main-bg">
-    <div class="shape-wrapper">
-        <img src="/wp-content/themes/Afnan/images/shape/34.svg" alt="" class="shape-one img-shape">
-    </div> <!-- /.shape-wrapper -->
-    <!-- <div class="shape-wrapper">
-					<img src="images/home/2.jpg" alt="" class="people">
-					<img src="images/home/3.jpg" alt="" class="people">
-					<img src="images/home/4.jpg" alt="" class="people">
-					<img src="images/home/5.jpg" alt="" class="people">
-					<img src="images/home/6.jpg" alt="" class="people">
-					<img src="images/home/7.jpg" alt="" class="people">
-				</div> -->
-    <div class="container">
-        <div class="theme-title-one text-center">
-            <div class="icon-box hide-pr">
-                <svg class="bg-shape" width="80px" height="80px">
-                    <defs>
-                        <linearGradient id="PSgrad_55" x1="0%" x2="0%" y1="100%" y2="0%">
-                            <stop offset="0%" stop-color=" rgb(184, 177, 162)" stop-opacity="1" />
-                            <stop offset="100%" stop-color="rgb(199, 185, 144)" stop-opacity="1" />
-                        </linearGradient>
-
-                    </defs>
-                    <path fill-rule="evenodd" fill="url(#PSgrad_55)"
-                        d="M40.000,-0.000 C62.091,-0.000 80.000,17.908 80.000,40.000 C80.000,62.091 62.091,80.000 40.000,80.000 C17.909,80.000 0.000,62.091 0.000,40.000 C0.000,17.908 17.909,-0.000 40.000,-0.000 Z" />
-                </svg>
-                <img src="/wp-content/themes/Afnan/images/icon/icon7.svg" alt="" class="icon">
-            </div>
-            <h2 class="main-title underline"><span>Tamer Group</span><br> <span>Was established in 1922 by Dr. Mohammed
-                    Saeed Tamer .</span></h2>
-        </div> <!-- /.theme-title-one -->
-
-        <div class="inner-container">
-            <div class="main-content">
-                <div class="item">
-                    <p>as the first pharmacy in the kingdom of Saudi Arabia, and he began to expand his field of work by
-                        importing and distributing medicines in the Kingdom. In 1963, Dr. Farouk Tamer and Mr. Mamoun
-                        Tamer
-                        took over the management and development of the company to occupy the forefront in the fields of
-                        medicine, medical equipment and consumer goods. Since 1985, the Tamer Group has continued to
-                        move
-                        forward and lead the third generation of sons to be able to produce medicines locally, and it
-                        has
-                        also been able to achieve strategic and important investments in the fields of trade, finance,
-                        insurance and information technology. The group’s activities focus on importing, distributing,
-                        promoting, marketing and manufacturing a wide range of products and services.</p>
-                    <h3 class="main-title">Tamer Group's <i> Mission .</i></h3>
-                    <p>: to achieve leadership at the regional level in the fields of medicines, health care, nutrition,
-                        health, beauty and well-being to meet the needs of society.</p>
-                    <!-- <h6 class="name">Rashed Ka.</h6>
-									<span class="designation">Senior Designer, Squre.</span> -->
-                </div> <!-- /.item -->
-                <!-- <div class="item">
-									<p>ommodo consequat. Duis aute irure dolor in reprehendert voluptate velit esse cillum dolore eu fugiat nulla Excepteu sint occaecat cupidat non proident, sunt in culpa qui officia deserunt mollit anim id est lrum.</p>
-									<h6 class="name">Zubayer al hasan</h6>
-									<span class="designation">Senior Developer, Envato.</span>
-								</div> /.item -->
-            </div>
-        </div> <!-- /.inner-container -->
-    </div> <!-- /.container -->
-</div> <!-- /.testimonial-section-classic -->
+                        <ul class="theme-list-item pb-70">
+                            <li><span><b>1.</b></span> To provide outstanding scientific research and studies from the
+                                discovery of the drug to its development and manufacture.
+                            </li>
+                            <li><span><b>2.</b></span> To generate and develop research and patents in the
+                                pharmaceutical
+                                field and turn them into locally manufactured pharmaceutical products in cooperation
+                                with
+                                universities, research centers, local and international pharmaceutical companies and
+                                factories.
+                            </li>
+                            <li><span><b>3.</b></span>Providing scientific and practical research services in the
+                                development and localization of the pharmaceutical industry.
+                            </li>
+                            <li><span><b>4.</b></span>Providing advice and recommendations related to the pharmaceutical
+                                industry.
+                            </li>
+                            <li><span><b>5.</b></span>Development of initiatives related to the pharmaceutical industry.
+                            </li>
+                            <li><span><b>6.</b></span>Conducting courses related to the discovery, development and
+                                manufacture of drugs.
+                            </li>
+                            <li><span><b>7.</b></span>Holding distinguished meetings and conferences in the
+                                pharmaceutical
+                                industry.
+                            </li>
+                            <li><span><b>8.</b></span>Participating in local and international meetings and conferences
+                                related to pharmaceutical research and pharmaceutical industries.
+                            </li>
+                            <li><span><b>9.</b></span>Contribute to the development of strategic plans that contribute
+                                to
+                                the development and localization of the pharmaceutical industry.
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6 order-lg-first">
+                        <div class="main-img-box pos-r hide-pr md-mt-50">
+                            <div class="img-one pos-r wow fadeInLeft" data-wow-duration="2s"><img
+                                    src=/wp-content/themes/Afnan/images/home/home-3.jpeg" alt="home-1"
+                                    style="height: 100%;"></div>
+                            <div class="box-shape pos-r wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.2s">
+                            </div>
+                            <div class="dot-shape pos-r wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.2s">
+                                <img src="/wp-content/themes/Afnan/images/shape/dot-shape.svg" alt=""></div>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- /.container -->
+        </div> <!-- /.inner-wrapper -->
+    </div> <!-- /.about-us-block-two -->
+</div>
