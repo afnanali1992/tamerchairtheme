0ce8be2406399286dd5bf7fd3ef45f257c17627d
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-07-10 05:02:21 +0300

    Single php

--- functions.php
+++ functions.php
@@ -139,33 +139,40 @@ the_post_thumbnail( array( 100, 100 ) ); // Other resolutions (height, width)
 
  
 
-add_shortcode( 'categorypost', 'cat_post' );
-
-function cat_post( $atts ) {
-
-    // attributes for shortcode
-   if ( isset( $atts['cat'] ) ) {$cats = $atts['cat'];} else {return;}
-   if ( isset( $atts['posts_per_page'] ) ) {$posts_per_page = $atts['posts_per_page'];} else {$posts_per_page = -1;}
-
-   // get the category posts
-   $category = get_category_by_slug( $cats );
-   if ( !is_object( $category ) ) {return;}
-   $args = array(
-        'cat' => $category->term_id,
-        'posts_per_page' => $posts_per_page
-   );
-   $posts = get_posts( $args );
-
-   // create the list output
-   if ( count( $posts ) > 0 ) {
-       foreach ( $posts as $post ) {
-           $link = get_permalink( $post->ID );
-           $title = $post->post_title;
-           $image = get_post_thumbnail( $post->ID,'thumbnail' );
-           $output .= '<div id="postrow-'.$post->ID.'" class="postrow">';
-           $output .= '<a class="postlink" href="'.$link.'">' . $image;
-           $output .= '<h5 class="posttitle">' . $title . '</h5></a></div>';
-       }
-   }
-   return $output;
+// create shortcode with parameters so that the user can define what's queried - default is to list all blog posts
+add_shortcode( 'list-posts', 'rmcc_post_listing_parameters_shortcode' );
+function rmcc_post_listing_parameters_shortcode( $atts ) {
+    ob_start();
+ 
+    // define attributes and their defaults
+    extract( shortcode_atts( array (
+        'type' => 'post',
+        'order' => 'date',
+        'orderby' => 'title',
+        'posts' => -1,
+        'category' => '',
+    ), $atts ) );
+ 
+    // define query parameters based on attributes
+    $options = array(
+        'post_type' => $type,
+        'order' => $order,
+        'orderby' => $orderby,
+        'posts_per_page' => $posts,
+        'category_name' => $category,
+    );
+    $query = new WP_Query( $options );
+    // run the loop based on the query
+    if ( $query->have_posts() ) { ?>
+        <ul class="clothes-listing">
+            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+            <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </li>
+            <?php endwhile;
+            wp_reset_postdata(); ?>
+        </ul>
+    <?php $myvariable = ob_get_clean();
+    return $myvariable;
+    }
 }
