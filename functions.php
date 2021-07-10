8f8c7318510d0a25dcd2c44d434db28bae80d68e
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-07-10 06:14:21 +0300

    Posts list function

--- functions.php
+++ functions.php
@@ -167,11 +167,11 @@ function rmcc_post_listing_parameters_shortcode( $atts ) {
      	<div class="our-blog version-five pt-110 pb-150 md-pb-120">
 		<div class="container">
         
-					<div class="masnory-blog-wrapper">
+					<div class="row masnory-blog-wrapper">
 						<div class="grid-sizer"></div>
             <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-            <div class="isotop-item">
-			<div class="blog-post-block-two mb-75 md-mb-50">
+            <div class="isotop-item col-lg-4 col-md-6">
+			<div class="single-team-member blog-post-block-two mb-75 md-mb-50">
             <div class="img-holder"><img src="<?php the_post_thumbnail_url(); ?>" alt=""></div>
             <div class="post">
 									<ul class="post-info">
