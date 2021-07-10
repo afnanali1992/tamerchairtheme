49f26d2f0eed57e9fa255bf632f8cf88d0baca66
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-07-10 06:56:51 +0300

    Posts list function

--- functions.php
+++ functions.php
@@ -195,3 +195,8 @@ function rmcc_post_listing_parameters_shortcode( $atts ) {
     return $myvariable;
     }
 }
+
+function custom_excerpt_length( $length ) {
+	return 20;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
