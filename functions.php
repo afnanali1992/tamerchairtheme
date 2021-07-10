7c530daa14e85dae3b77d31642ea55f722816822
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-07-10 03:46:36 +0300

    image feature Posts

--- functions.php
+++ functions.php
@@ -127,7 +127,15 @@ function load_stylesheets()
 // add_action( 'after_setup_theme', 'register_navwalker' );
 
 
-
+	
+add_theme_support( 'post-thumbnails' );
+the_post_thumbnail(); // Without parameter ->; Thumbnail
+the_post_thumbnail( 'thumbnail' ); // Thumbnail (default 150px x 150px max)
+the_post_thumbnail( 'medium' ); // Medium resolution (default 300px x 300px max)
+the_post_thumbnail( 'medium_large' ); // Medium-large resolution (default 768px x no height limit max)
+the_post_thumbnail( 'large' ); // Large resolution (default 1024px x 1024px max)
+the_post_thumbnail( 'full' ); // Original image resolution (unmodified)
+the_post_thumbnail( array( 100, 100 ) ); // Other resolutions (height, width)
 
 function cat_post($atts){
 
