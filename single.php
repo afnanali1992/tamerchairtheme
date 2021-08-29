a955f13fb1e32610c11caa5d22b486dfd5ef44c7
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-08-29 15:55:43 +0300

    list-publication

--- single.php
+++ single.php
@@ -1,22 +1,28 @@
 <?php get_header();?>
-  
+
 
 <?php if(has_post_thumbnail());?>
- 
-			<div class="inner-banner pos-r banner-bg bg-style-two" style="background-image:  url(<?php the_post_thumbnail_url(); ?>); ">
-				<div class="opacity">
-					<div class="container">
-						<p><?php $category = get_the_category();
-$firstCategory = $category[0]->cat_name; echo $firstCategory;?></p>
-						<h2> <?php the_title(); ?> </h2>
-					</div>
+
+<div class="our-blog blog-details blog-details-fg pb-200 md-pb-120">
+	<div class="blog-hero-banner" style="background-image:  url(<?php the_post_thumbnail_url(); ?>); ">
+
+		<div class="blog-custom-container">
+			<p><?php $category = get_the_category(); $firstCategory = $category[0]->cat_name; echo $firstCategory;?></p>
+			<h2 class="blog-title"> <?php the_title(); ?> </h2>
+		</div>
+
+	</div> <!-- /.inner-banner -->
+	<div class="blog-fg-data">
+		<div class="post-data">
+			<div class="blog-custom-container">
+				<div class="custom-container-bg">
+
+
+					<?php the_content();?>
+
 				</div>
-			</div> <!-- /.inner-banner -->
-            <div class="our-blog blog-details pt-150 pb-200 md-pt-120 md-pb-120">
-				<div class="container">
-				 
-<?php the_content();?>
- 
-</div>
+			</div>
+		</div>
+	</div>
 </div>
 <?php get_footer();?>
