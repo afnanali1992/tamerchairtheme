87c69a2facde0b8fa1ccbdecaa76ffc6578af5f1
Afnan Bayazid <afnanbayazid@Afnans-MacBook-Pro.local>
2021-08-29 12:36:06 +0300

    list-publication

--- functions.php
+++ functions.php
@@ -237,11 +237,10 @@ function publication_fun( $atts ) {
                                 <div class="date"><?php the_time('F j, Y'); ?></div>
                             </div>
                         </div>
-                        <a href="<?php echo get_post_meta(get_the_ID(), "url-post", true); ?>" class="title"><?php the_title(); ?></a>
+                        <a href="<?php $key_name = get_post_custom_values($key = 'url-post'); echo $key_name[0]; ?>" class="title"><?php the_title(); ?></a>
                         <p><?php the_excerpt(); ?></p>
-                        <p><?php $key_name = get_post_custom_values($key = 'url-post');
-echo $key_name[0]; ?></p>
-                      
+
+
                     </div>
                 </div>
             </div>
